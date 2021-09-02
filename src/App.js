58cd1e54751df27400ed31a00a390df95bdf469a
Fer <fjuare34@gmail.com>
2021-09-02 08:34:54 -0300

    2 cambio

--- src/App.js
+++ src/App.js
@@ -10,41 +10,27 @@ import './App.css';
 import HomePage from "./pages/Home";
 import CategoryPage from "./pages/Category";
 import NewsPage from "./pages/News";
+import NavBar from "./components/NavBar";
 
 export default function App() {
-  return (
+  return (){
+    <>
     <Router>
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/category/business">Business</Link>
-            </li>
-            <li>
-              <Link to="/category/sports">Sports</Link>
-            </li>
-          </ul>
-        </nav>
-
-        {/* A <Switch> looks through its children <Route>s and
-            renders the first one that matches the current URL. */}
+        <NavBar />
         <Switch>
-        <Route path="/news/:newsId">
+          <Route path="/news/:newsId">
             <NewsPage />
-          <Route path="/category/:categorySlug">
-            <CategoryPage />
-          </Route>
-          <Route path="/">
-            <HomePage />
-          </Route>
+            <Route path="/category/:categorySlug">
+              <CategoryPage />
+            </Route>
+            <Route path="/">
+              <HomePage />
+            </Route>
         </Switch>
       </div>
     </Router>
-  );
-}
-
-
-
+      )
+      </>
+    }
+  }
